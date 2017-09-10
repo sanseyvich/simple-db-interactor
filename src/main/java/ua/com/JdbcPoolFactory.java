62dae4f5b941b18a0e7b56101a424ba933016eb4
Sanseyvich <sanseyvich@Sanseyvichs-MacBook-Pro.local>
2017-09-10 23:27:46 +0300

    Redo JdbcTemplateFactory to singleton

--- src/main/java/ua/com/JdbcPoolFactory.java
+++ src/main/java/ua/com/JdbcPoolFactory.java
@@ -1,7 +1,5 @@
 package ua.com;
 
-
-import org.apache.tomcat.jdbc.pool.PoolConfiguration;
 import org.apache.tomcat.jdbc.pool.PoolProperties;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -10,8 +8,14 @@ import javax.sql.DataSource;
 /**
  * Created by sanseyvich on 9/9/17.
  */
-public class JdbcTemplateFactory {
+public class JdbcPoolFactory {
+    //singleton
+    private static JdbcTemplate jdbcTemplate;
+
     public JdbcTemplate createMysqlJdbcPool() {
+        if (this.jdbcTemplate != null) {
+            return this.jdbcTemplate;
+        }
         PoolProperties p = new PoolProperties();
         p.setUrl("jdbc:mysql://localhost:3306/test_1?serverTimezone=UTC");
         p.setDriverClassName("com.mysql.cj.jdbc.Driver");
@@ -29,7 +33,7 @@ public class JdbcTemplateFactory {
                         "org.apache.tomcat.jdbc.pool.interceptor.StatementFinalizer");
 
         DataSource dataSource = new org.apache.tomcat.jdbc.pool.DataSource(p);
-
-        return new JdbcTemplate(dataSource);
+        jdbcTemplate = new JdbcTemplate(dataSource);
+        return jdbcTemplate;
     }
 }
